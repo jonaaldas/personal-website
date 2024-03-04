9dc6342c0a47b8f055a8ea720b882241525e2625
jonaaldas <jonaaldas@gmail.com>
2024-03-04 15:55:03 -0500

    deleted old client

--- client1/client/src/components/Loader.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div class="w-full h-screen bg-black flex justify-center items-center">
-		<div class="text-[120px] font-sans text-white">ALDAS</div>
-	</div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style lang="scss" scoped></style>
--- client1/client/src/router/index.js
+++
@@ -1,8 +0,0 @@
-import {createRouter, createWebHistory} from 'vue-router/auto';
-import App from '@/App.vue';
-
-const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL)
-});
-
-export default router;
